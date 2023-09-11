7a7754b82d0c98451e14d59008025bc09e0282ab
Collins Wakahenya <wakahenyacollins@gmail.com>
2023-09-11 16:30:01 +0300

    Done with assignment

--- lib/debug.py
+++ lib/debug.py
@@ -3,6 +3,10 @@
 from config import CONN, CURSOR
 from song import Song
 
-
 if __name__ == '__main__':
+    # Import 'ipdb' and set a trace point
     import ipdb; ipdb.set_trace()
+
+    # Your code here
+    # You can interactively debug your code after this point
+
